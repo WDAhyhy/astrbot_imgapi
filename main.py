318234dadc84656172e1f9f5b7fe4ab97b1d573a
WDAhyhy <1485826483@qq.com>
2025-02-16 14:00:37 +0800

    初次发布

---
+++ main.py
@@ -0,0 +1,55 @@
+from astrbot.api.message_components import *
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.star import Context, Star, register
+import aiohttp
+
+
+@register("fish_apiimg", "案板上的鹹魚", "从API获取图片。双路径：使用 /img 和/imgh 获取。(自用)", "1.0")
+class SetuPlugin(Star):
+    def __init__(self, context: Context, config: dict):
+        super().__init__(context)
+        self.config = config
+        self.api_url = config.get("api_url", "")
+        self.h_url = self.api_url +'/h'
+        self.nh_url = self.api_url +'/nh'
+    @filter.command("img")
+    async def get_setu(self, event: AstrMessageEvent):
+        # 检查是否配置了API URL
+        if not self.api_url:
+            yield event.plain_result("\n请先在配置文件中设置API地址")
+            return
+
+        # 创建一个不验证SSL的连接上下文
+        ssl_context = aiohttp.TCPConnector(verify_ssl=False)
+        async with aiohttp.ClientSession(connector=ssl_context) as session:
+            try:
+
+                # 构建消息链
+                chain = [
+                    Image.fromURL(self.nh_url)  # 从URL加载图片
+                ]
+
+                yield event.chain_result(chain)
+            except Exception as e:
+                yield event.plain_result(f"\n请求失败: {str(e)}")
+
+    @filter.command("imgh")
+    async def get_setu(self, event: AstrMessageEvent):
+        # 检查是否配置了API URL
+        if not self.api_url:
+            yield event.plain_result("\n请先在配置文件中设置API地址")
+            return
+
+        # 创建一个不验证SSL的连接上下文
+        ssl_context = aiohttp.TCPConnector(verify_ssl=False)
+        async with aiohttp.ClientSession(connector=ssl_context) as session:
+            try:
+
+                # 构建消息链
+                chain = [
+                    Image.fromURL(self.h_url)  # 从URL加载图片
+                ]
+
+                yield event.chain_result(chain)
+            except Exception as e:
+                yield event.plain_result(f"\n请求失败: {str(e)}")
