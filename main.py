dcb195380fbf6a57d06d20703307d2ca9f0d18b7
WDAhyhy <1485826483@qq.com>
2025-02-19 20:42:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,12 +29,12 @@ class SetuPlugin(Star):
                     # 构建消息链
                     chain = [
 
+                        Plain(f"正在发送~~~({i + 1}/{n})"),
+                        Image.fromURL(self.nh_url)  # 从URL加载图片
 
-                        Image.fromURL(self.nh_url),  # 从URL加载图片
-                        Plain(f"正在发送~~~({i + 1}/{n})")
                     ]
                     yield event.chain_result(chain)
-                    await asyncio.sleep(3)
+                    await asyncio.sleep(1)
                 except Exception as e:
                     yield event.plain_result(f"\n请求失败: {str(e)}")
 
